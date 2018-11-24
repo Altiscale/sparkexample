0f0a4d66e85c66ec7ce2fdc5e5532f9c2a2d06a6
Ganesh Ranganathan <ganeshr@altiscale.com>
2018-11-24 09:35:52 -0800

    GREEN-700:cutting 2.3.2 version for rpm. Note changes in pom.xml in terms of deendent versions for spark-2.3 profile and hadoop27-provided profiles for any build or test issues

---
+++ test_sparksql_shell.sh
@@ -0,0 +1,70 @@
+#!/bin/sh 
+
+# Run the test case as alti-test-01
+# /bin/su - alti-test-01 -c "./test_spark/test_spark_shell.sh"
+spark_home=${SPARK_HOME:='/opt/spark'}
+if [ ! -d "$spark_home" ] ; then
+  >&2 echo "fail - $spark_home does not exist, please check you Spark installation or SPARK_HOME env variable, exinting!"
+  exit -2
+else
+  echo "ok - applying Spark home $spark_home"
+fi
+
+source $spark_home/test_spark/init_spark.sh
+source $spark_home/test_spark/deploy_hive_jar.sh
+
+# Default SPARK_CONF_DIR is already checked by init_spark.sh
+spark_conf=${SPARK_CONF_DIR:-"/etc/spark"}
+if [ ! -d "$spark_conf" ] ; then
+  >&2 echo "fail - $spark_conf does not exist, please check you Spark installation or your SPARK_CONF_DIR env value, exiting!"
+  exit -2
+else
+  echo "ok - applying spark config directory $spark_conf"
+fi
+
+spark_version=$SPARK_VERSION
+if [ "x${spark_version}" = "x" ] ; then
+  >&2 echo "fail - SPARK_VERSION can not be identified or not defined, please review SPARK_VERSION env variable? Exiting!"
+  exit -2
+fi
+
+curr_dir=`dirname $0`
+curr_dir=`cd $curr_dir; pwd`
+spark_test_dir=""
+
+if [ ! -f "$spark_test_dir/pom.xml" ] ; then
+  echo "warn - correcting test directory from $spark_test_dir to $curr_dir"
+  spark_test_dir=$curr_dir
+fi
+
+pushd `pwd`
+cd $spark_home
+
+echo "ok - testing spark SQL shell with simple queries"
+
+app_name=`grep "<artifactId>.*</artifactId>" $spark_test_dir/pom.xml | cut -d">" -f2- | cut -d"<" -f1  | head -n 1`
+app_ver=`grep "<version>.*</version>" $spark_test_dir/pom.xml | cut -d">" -f2- | cut -d"<" -f1 | head -n 1`
+
+if [ ! -f "$spark_test_dir/${app_name}-${app_ver}.jar" ] ; then
+  >&2 echo "fail - $spark_test_dir/${app_name}-${app_ver}.jar test jar does not exist, cannot continue testing, failing!"
+  exit -3
+fi
+
+spark_event_log_dir=$(grep 'spark.eventLog.dir' ${spark_conf}/spark-defaults.conf | tr -s ' ' '\t' | cut -f2)
+
+queue_name=""
+# queue_name="--queue interactive"
+queue_name=""
+./bin/spark-sql --verbose $queue_name \
+  --master yarn --deploy-mode client \
+  --conf spark.yarn.am.extraJavaOptions="-Djava.library.path=$HADOOP_HOME/lib/native" \
+  --conf spark.eventLog.dir=${spark_event_log_dir}/$USER 
+
+if [ $? -ne "0" ] ; then
+  >&2 echo "fail - testing shell for SparkSQL on HiveQL/HiveContext failed!!"
+  exit -4
+fi
+
+popd
+
+exit 0
