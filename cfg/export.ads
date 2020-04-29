f6773d6ab8c689acc94252a7ef7f3676381cd60f
Andrew Lee <andrew.lee02@sap.com>
2020-04-29 16:31:19 -0700

    Fix export path

--- cfg/export.ads
+++ cfg/export.ads
@@ -3,7 +3,7 @@ artifacts builderVersion: "1.1", {
   group "com.sap.bds.ats-altiscale", {
 
     artifact "oozie", {
-      file "$gendir/src/sparkexample_rpmbuild/rpm/alti-spark-${buildVersion}-example.rpm"
+      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-${buildVersion}-example.rpm"
     }
   }
 }
