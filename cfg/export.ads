9b166c5e10e79c352e4c301361362b8e49a29ad4
Andrew Lee <andrew.lee02@sap.com>
2020-04-29 16:41:26 -0700

    Fix export path and groupId value

--- cfg/export.ads
+++ cfg/export.ads
@@ -1,8 +1,7 @@
 artifacts builderVersion: "1.1", {
 
   group "com.sap.bds.ats-altiscale", {
-
-    artifact "oozie", {
+    artifact "sparkexample", {
       file "$gendir/src/spark_rpmbuild/rpm/alti-spark-${buildVersion}-example.rpm"
     }
   }
