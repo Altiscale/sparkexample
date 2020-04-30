80413bb64cb21061df17d654c9dfd7d60d2be6d2
Andrew Lee <andrew.lee02@sap.com>
2020-04-29 18:21:05 -0700

    Fix export to fix value

--- cfg/export.ads
+++ cfg/export.ads
@@ -2,7 +2,7 @@ artifacts builderVersion: "1.1", {
 
   group "com.sap.bds.ats-altiscale", {
     artifact "sparkexample", {
-      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-${buildVersion}-example.rpm"
+      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-3.0.0-example.rpm"
     }
   }
 }
